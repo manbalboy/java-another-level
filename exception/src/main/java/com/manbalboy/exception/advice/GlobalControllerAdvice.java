bf6e23beee34a8652e039797e9f79f36ccd6cb4e
manbalboy <manbalboy@hanmail.net>
2021-09-22 15:40:43 +0900

    validation response exception modify - #9

--- exception/src/main/java/com/manbalboy/exception/advice/GlobalControllerAdvice.java
+++ exception/src/main/java/com/manbalboy/exception/advice/GlobalControllerAdvice.java
@@ -1,13 +1,15 @@
 package com.manbalboy.exception.advice;
 
 
+import com.manbalboy.exception.controller.ApiController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = ApiController.class)
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
@@ -23,11 +25,15 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity exception(MethodArgumentNotValidException e) {
-        System.out.println("===============");
+        System.out.println("===============MethodArgumentNotValidException");
         System.out.println(e.getMessage());
         System.out.println("===============");
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
+
+
+
 
 }
