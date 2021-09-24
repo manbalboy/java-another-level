28903333cb1dc0bf2ec6c551c2a04c7a972be6ae
manbalboy <manbalboy@hanmail.net>
2021-09-24 17:05:48 +0900

    spring-junit - 활용 - #19

---
+++ spring-junit-test/src/main/java/com/manbalboy/springjunittest/component/MarketApi.java
@@ -0,0 +1,10 @@
+package com.manbalboy.springjunittest.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+    public int connect() {
+        return 110;
+    }
+}
