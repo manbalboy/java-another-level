c25f8d21433c468245b0556f174a37cf3323485f
manbalboy <manbalboy@hanmail.net>
2021-09-25 16:46:00 +0900

    JPA save findAll - #25

--- part4/jpa/src/main/java/com/manbalboy/jpa/bookmanaber/domain/User.java
+++ part4/jpa/src/main/java/com/manbalboy/jpa/bookmanaber/domain/User.java
@@ -1,13 +1,31 @@
 package com.manbalboy.jpa.bookmanaber.domain;
 
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Data
+@Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String email;
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
+
+
 }
